4eebf4d5a7984d66c300faf52ee78037e0966f1a
Shubham <shubhamkaushik91@gmail.com>
2025-11-24 15:03:34 +0530

    Updated Files

--- user-service/src/main/java/com/shopease/user/config/CorsConfig.java
+++ user-service/src/main/java/com/shopease/user/config/CorsConfig.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
                         .allowedMethods("*")
@@ -22,3 +22,4 @@ public class CorsConfig {
         };
     }
 }
+
